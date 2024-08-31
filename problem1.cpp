97a292a28d0224ee9fb3c742d55c779ce4f6d737
Mahishashur <yashimanshusingh@gmail.com>
2024-08-31 22:50:34 +0530

    code

---
+++ problem1.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string word3;
+        int i;
+        for (i = 0; i < word1.length() || i < word2.length(); i++) {
+            if (i < word1.length()) word3 += word1[i];
+            if (i < word2.length()) word3 += word2[i];
+        }
+        return word3;
+    }
+};
