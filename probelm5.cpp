43056336242fab0e2c7f39d5af49fb9e89ef0886
Mahishashur <yashimanshusingh@gmail.com>
2024-09-16 15:19:11 +0530

    code

--- probelm5.cpp
+++ probelm5.cpp
@@ -1,1 +1,16 @@
 //To Lower Case
+
+class Solution {
+ public:
+  string toLowerCase(string str) {
+
+    const int convert = 'A' - 'a';
+
+    for (char& x : str)
+    
+      if (x >= 'A' && x <= 'Z')
+        x -= convert;
+
+    return str;
+  }
+};
