f517ede06709f35671a2c0fb93598766a2d86ad1
Mahishashur <yashimanshusingh@gmail.com>
2024-10-09 11:14:41 +0530

    code

---
+++ Problem7.cpp
@@ -0,0 +1,11 @@
+// Richest customer Wealth
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int ans = 0;
+        for (auto& v : accounts) {
+            ans = max(ans, accumulate(v.begin(), v.end(), 0));
+        }
+        return ans;
+    }
+};
